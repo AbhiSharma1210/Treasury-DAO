b8d86442783e0da77628739a9965eefba0ac79bc
Abhinav Sharma <abhi5237@gmail.com>
2024-11-02 23:00:41 +0530

    minor bug fixes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import chestLogo from '../images/chestLogo.png';
+import chestLogo from '../../images/chestLogo.png';
 
 const Navbar = () => {
     return (
-        <nav className="w-full bg-opacity-80 backdrop-filter backdrop-blur-lg bg-blue-900 px-6 py-3 flex justify-between items-center">
+        <nav className="w-full bg-opacity-80 backdrop-filter backdrop-blur-lg bg-slate-950 px-6 py-3 flex justify-between items-center">
             <div className="flex items-center space-x-2">
                 <img
                     src={chestLogo}
@@ -12,7 +12,7 @@ const Navbar = () => {
                 />
             </div>
             <div>
-                <button className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                <button className="text-white bg-emerald-600 py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-emerald-900">
                     Login
                 </button>
             </div>
